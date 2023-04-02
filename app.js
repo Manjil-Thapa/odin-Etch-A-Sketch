0e5d10dcf6312eb5d878c5685673e7d5fad217d0
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-04-03 01:57:23 +1000

    added raninbow sketch

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// Global default color
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function () {
-  createBoard(32);
+  createBoard(16);
+
+  // Variable for a button
+  let gridSizeBtn = document.querySelector("#grid-size");
+  gridSizeBtn.addEventListener("click", function () {
+    let size = gridSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
@@ -12,6 +22,37 @@ function createBoard(size) {
   for (let i = 0; i < pixels; i++) {
     let divs = document.createElement("div");
     divs.classList.add("pixel");
+    // Draw
+    divs.addEventListener("mouseover", pixelColor);
     board.insertAdjacentElement("beforeend", divs);
   }
+}
+
+// Promt for canvas dimensions
+const gridSize = () => {
+  let input = prompt("Insert a number between 1 to 100 for your canvas size");
+  if (!input || input < 0 || input > 100) {
+    document.querySelector("#prompt-msg").textContent =
+      "Please provide a number from 1 to 100";
+  }
+  if (input <= 0 || input > 100) {
+    document.querySelector("#prompt-msg").textContent =
+      "You can only insert a number in the range of 1 to 100";
+  } else {
+    document.querySelector("#prompt-msg").textContent = "Enjoy!";
+    return input;
+  }
+};
+
+// Pixel colors
+function pixelColor() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
 }
